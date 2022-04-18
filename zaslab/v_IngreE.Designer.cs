bc57211a4bf6b8b02a9ce09c8cafc9a45cda95a6
na1ht <ikristhian6@gmail.com>
2022-04-18 16:45:41 -0600

    sosputa

--- zaslab/v_IngreE.Designer.cs
+++ zaslab/v_IngreE.Designer.cs
@@ -147,6 +147,7 @@
             this.button1.TabIndex = 23;
             this.button1.Text = "Importar Excel";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label5
             // 
